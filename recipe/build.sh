1273b294d20893e9f0391d9189ae2d990d7197d2
dingye <dingye@westlake.edu.cn>
2021-08-20 15:19:36 +0800

    Update

---
+++ recipe/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir build
+cd build
+if [ ${float_prec} == "high" ]; then
+    export PREC_DEF="-DHIGH_PREC"
+fi
+
+cmake -D CMAKE_BUILD_TYPE=Release -D BUILD_LIB=on -D BUILD_SHARED_LIBS=on -DOPENMM_DIR=${PREFIX} -DDEEPMD_DIR=${PREFIX} -DTENSORFLOW_DIR=${PREFIX}  -DCMAKE_CXX_FLAGS="-DHIGH_PREC -I${PREFIX}/include -L${PREFIX}/lib -Wl,--no-as-needed -lrt -ldeepmd_op -ldeepmd -ldeepmd_cc -ltensorflow_cc -ltensorflow_framework -Wl,-rpath=${PREFIX}/lib" ..
+make #-j${NUM_CPUS}
+make install
+make test
+make PythonInstall
+cd ..
