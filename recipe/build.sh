0f994b832a3d73d2d7b1ef72d90abb9fa6c4891a
dingye <dingye@westlake.edu.cn>
2021-09-15 22:11:48 +0800

    Fix build bugs for plugin.

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,8 +7,10 @@ if [ ${float_prec} == "high" ]; then
 fi
 
 cmake -D CMAKE_BUILD_TYPE=Release -D BUILD_LIB=on -D BUILD_SHARED_LIBS=on -DOPENMM_DIR=${PREFIX} -DDEEPMD_DIR=${PREFIX} -DTENSORFLOW_DIR=${PREFIX}  -DCMAKE_CXX_FLAGS="-DHIGH_PREC -I${PREFIX}/include -L${PREFIX}/lib -Wl,--no-as-needed -lrt -ldeepmd_op -ldeepmd -ldeepmd_cc -ltensorflow_cc -ltensorflow_framework -Wl,-rpath=${PREFIX}/lib" ..
+export LD_LIBRARY_PATH=${PREFIX}
+
 make #-j${NUM_CPUS}
 make install
 make test
 make PythonInstall
-cd ..
+
